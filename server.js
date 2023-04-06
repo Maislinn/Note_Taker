e6da8d40164351f9cb45b04472593ef1e1960542
Maislinn <maislinn.helfer@gmail.com>
2023-04-06 11:22:00 -0700

    debugging server.js

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 //Adding Server.JS
 // Requiring packages 
-const htmlRoutes = require('./routes/htmlRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
-const path = require('path');
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+const api = require('./routes/notes.js');
+const html = require('./routes/html.js');
+//const path = require('path');
+
+//const apiRoutes = require('./routes/apiRoutes');
 
 //Assign a port 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
 
 //Middleware for parsing JSON and urlencoded form data 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
 
