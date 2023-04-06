333141c3783e7c7fdce9a7a607a568dcbe2df73a
Maislinn <maislinn.helfer@gmail.com>
2023-04-06 12:52:17 -0700

    adding in notes.js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,33 +1,33 @@
-const router = require('express').Router();
-//const apiRoutes = require('./apiRoutes');
-const {readFromFile, readAndAppend, writeToFile} = require('../helpers/node-fsutils.js');
-const path = require('path');
+// const router = require('express').Router();
+// //const apiRoutes = require('./apiRoutes');
+// const {readFromFile, readAndAppend, writeToFile} = require('../helpers/node-fsutils.js');
+// const path = require('path');
 
-// GET Route for retrieving all the notes
-router.get('/notes', (req, res) => {
-   console.log('api routes')
-   readFromFile(path.join(__dirname, '../db/db.json')).then((data) => res.json(JSON.parse(data)));
-});
+// // GET Route for retrieving all the notes
+// router.get('/notes', (req, res) => {
+//    console.log('api routes')
+//    readFromFile(path.join(__dirname, '../db/db.json')).then((data) => res.json(JSON.parse(data)));
+// });
 
-// POST Route for a new note
-router.post('/notes', (req, res) => {
-    console.log(req.body);
+// // POST Route for a new note
+// router.post('/notes', (req, res) => {
+//     console.log(req.body);
 
-    const { title, text } = req.body;
+//     const { title, text } = req.body;
 
-    if (title && text) {
+//     if (title && text) {
     
-        const newNote = {
-            title,
-            text,
-        };
+//         const newNote = {
+//             title,
+//             text,
+//         };
 
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully`);
-    } else {
-        res.error('Error in adding note');
-    }
-});
+//         readAndAppend(newNote, './db/db.json');
+//         res.json(`Note added successfully`);
+//     } else {
+//         res.error('Error in adding note');
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
 
