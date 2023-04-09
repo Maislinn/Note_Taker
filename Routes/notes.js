3db03b617dd80cf439fdbc40671197520c672605
Maislinn <maislinn.helfer@gmail.com>
2023-04-09 12:02:35 -0700

    updating routes

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,39 +1,35 @@
-const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend, writeToFile } = require('../helpers/node-fsutils.js');
 
-notes.get('/', (req, res) => {
-    console.info('get request received');
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+ 
+
+const router = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const path = require('path')
+
+// GET Route for retrieving all the notes
+router.get('/notes', (req, res) => {
+  console.log("test route")
+  readFromFile(path.join(__dirname,'../db/db.json')).then((data) => {
+        console.log(JSON.parse(data));
+        res.json(JSON.parse(data));
+       
+   });
+  });
+
+router.post('/notes', (req, res) => {
+
+  const { title, text } = req.body;
+  
+  if (req.body) {
+    const newNote = {
+     title,
+     text
+    };
+
+    readAndAppend(newNote, path.join(__dirname,'../db/db.json'));
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
-notes.post('/', (req, res) => {
-    console.info('post request received');
-    consple.log(req.body);
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuidv4(),
-        };
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully`);
-    } else {
-        res.error('Error in adding note');
-        console.error('Error in adding note');
-    }
-});
-
-notes.delete('/:id', (req, res) => {
-    const noteId = req.params.id;
-    readFromFile('./db/db.json')
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            const result = json.filter((note) => note.note_id !== noteId);
-            writeToFile('./db/db.json', result);
-            res.json(`Note ${noteId} has been deleted 🗑️`);
-        });
-});
-
-module.exports = notes;
+module.exports = router;
