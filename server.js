3a7df366247f2b43e014cc0d057e6368eda2eefb
Maislinn <maislinn.helfer@gmail.com>
2023-04-09 11:49:13 -0700

    updating server.js file

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 // Requiring packages 
 //const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
-const api = require('./routes/apiRoutes.js');
-const html = require('./routes/htmlRoutes.js');
-//const path = require('path');
-
+// const api = require('./routes/apiRoutes.js');
+// const html = require('./routes/htmlRoutes.js');
+// //const path = require('path');
 //const apiRoutes = require('./routes/apiRoutes');
+const path = require('path');
+const api = require('./routes/index');
 
 //Assign a port 
 const PORT = process.env.PORT || 3001;
@@ -18,12 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api);
-app.use('/', html);
+//app.use('/', html);
 
 // // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+ app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+ );
+
+ app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
